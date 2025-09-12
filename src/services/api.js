7e6f5aab3e41c75fc434a6fc9374cb368d407eed
Daniel <danielmello0110@gmail.com>
2025-09-12 14:01:03 -0300

    fix: update baseURL for API connection

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bigcorp-backend.onrender.com/',
+  baseURL: 'back-fedconnect-ocrmr.ondigitalocean.app/',
 });
 
 // Intercepta todas as requisições Axios
