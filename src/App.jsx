aea7dcc7b1de28473a3a52b0d572e5c337fc9766
Ingryd Aylana <ingrydaylana@gmail.com>
2025-11-26 14:33:52 -0300

    Feature: Implementação apresentação comercial

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,8 @@ import Acompanhamento from './components/Comercial/DashboardComercial'
 import AgendaComercial from './components/Comercial/AgendaComercial'
 import Financeiro from './components/Comercial/Financeiro'
 
+import Produtos from './components/Produtos/Produtos';
+
 import Conta from './components/Dropdown/dropItens/conta';
 import Config from './components/Dropdown/dropItens/Configuracoes';
 import Cadastro from './components/Dropdown/dropItens/Cadastro';
@@ -91,6 +93,7 @@ function AppRoutes() {
             <Route path="/acompanhamento" element={<Acompanhamento />} />
             <Route path="/agenda-comercial" element={<AgendaComercial />} />
             <Route path="/financeiro" element={<Financeiro />} />
+            <Route path="/produtos" element={<Produtos />} />
           </Route>
         </Route>
       </Routes>
